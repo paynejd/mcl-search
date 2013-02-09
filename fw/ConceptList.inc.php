c3fb2bb7a02f13089979bc9f519e4e8308d6dd4b
Jonathan Payne <paynejd@gmail.com>
2013-02-09 14:09:05 -0500

    Added union and intersect methods to ConceptList

--- fw/ConceptList.inc.php
+++ fw/ConceptList.inc.php
@@ -74,4 +74,48 @@ class ConceptList
 		}
 		return $c;
 	}
+
+
+	/**
+	 * Return a new concept list object that is the intersection
+	 * of the current object and the passed concept list.
+	 */
+	public function intersect(ConceptList $cl) 
+	{
+		$cl_intersect = new ConceptList(null);
+		foreach (array_keys($this->arr_concept_id) as $dict_db) {
+			foreach ($this->arr_concept_id[$dict_db] as $concept_id) {
+				if ($cl->isMember($dict_db, $concept_id)) {
+					$cl_intersect->addConcept($dict_db, $concept_id);
+				}
+			}
+		}
+		return $cl_intersect;
+	}
+
+	/**
+	 * Return a new concept list object that is the union
+	 * of the current object and the passed concept list.
+	 */
+	public function union(ConceptList $cl)
+	{
+		$cl_union = new ConceptList(null);
+
+		// this concept list
+		foreach (array_keys($this->arr_concept_id) as $dict_db) {
+			foreach ($this->arr_concept_id[$dict_db] as $concept_id) {
+				$cl_union->addConcept($dict_db, $concept_id);
+			}
+		}
+
+		// the passed concept list
+		$arr_cl2_concept_id = $cl->getArray();
+		foreach (array_keys($arr_cl2_concept_id) as $dict_db) {
+			foreach ($arr_cl2_concept_id[$dict_db] as $concept_id) {
+				$cl_union->addConcept($dict_db, $concept_id);
+			}
+		}
+
+		return $cl_union;
+	}
 }
