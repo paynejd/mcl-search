bb141fc79189a08bea8ef1948d18eb2dd53db015
Jonathan Payne <paynejd@gmail.com>
2013-02-12 08:45:50 -0500

    Proxy for AJAX search requests

---
+++ proxy.php
@@ -0,0 +1,33 @@
+<?php
+
+// Set the url
+	$url = $_GET['url'];
+	unset($_GET['url']);
+
+// Convert the search query (if necessary)
+	//if (isset($_GET['q'])) {
+	//	$url .
+	//}
+	//unset($_GET['q']);
+
+// Append the rest of the parameters
+	if (count($_GET)) {
+		$url .= '?';
+		foreach ($_GET as $k => $v) {
+			$url .= urlencode($k) . '=' . urlencode($v) . '&';
+		}
+	}
+
+// Perform the query
+	$ch   =  curl_init($url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$json_search =  curl_exec($ch);	// Website returns json
+	curl_close($ch);
+	if (!$json_search) {
+		var_dump($json_search);
+		trigger_error('curl 1 failed');
+		exit();
+	}
+	echo $url, var_dump( $json_search );
+
+?>
